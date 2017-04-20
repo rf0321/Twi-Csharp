522a8f4fe626606a9e305300ce481a61f0619fe9
Kekun321R <jkekun@gmail.com>
2017-04-20 22:44:22 +0900

    Add files via upload
    とりあえず補間

---
+++ TweetApp/TweetApp/API.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+using System.IO;
+using System.Web;
+
+//using System.Text;
+//using System.Text.RegularExpressions;
+using System.Xml;
+namespace System.Twitter
+{
+    class TwitterAPI
+    {
+        readonly string t_ConsumerKey; //APIたたくのにいるトークンキーたち
+        readonly string t_ConsumerSecret;
+        readonly string t_AccessToken;
+        readonly string t_AccessTokenSecret;
+        const string TwitterApiBaseUrl = "https://api.twitter.com/1.1/";
+      
+
+        readonly HMACSHA1 SignatureHashCode;
+        readonly DateTime UnixTimeStamp = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public  TwitterAPI(string t_ConsumerKey, string t_ConsumerSecret, string t_AccessToken, string t_AccessTokenSecret)
+        {
+            this.t_ConsumerKey = t_ConsumerKey;
+            this.t_ConsumerSecret = t_ConsumerSecret;
+            this.t_AccessToken = t_AccessToken;
+            this.t_AccessTokenSecret = t_AccessTokenSecret;
+
+            SignatureHashCode = new HMACSHA1(new ASCIIEncoding().GetBytes(string.Format("{0}&{1}", t_ConsumerSecret,t_AccessTokenSecret)));         
+        }
+        public Task<string>Tweet(string text)
+        {
+            var DATA = new Dictionary<string, string>
+            {
+                {"stauts",text },
+                {"trim_user","1" }
+            };
+            return SEND_REQUEST("statuses/update.json",DATA);
+        }
+        Task<string>SEND_REQUEST(string URL, Dictionary<string, string> DATA)
+        {
+            var Full_URL = TwitterApiBaseUrl + URL;
+            var timestamp = (int)((DateTime.UtcNow-UnixTimeStamp).TotalSeconds);
+             DATA.Add("oauth_consumer_key", t_ConsumerKey);
+             DATA.Add("oauth_signature_method", "HMAC-SHA1");
+             DATA.Add("oauth_timestamp", timestamp.ToString());
+             DATA.Add("oauth_nonce", "a");
+             DATA.Add("oauth_token", t_AccessToken);
+             DATA.Add("oauth_version", "1.0");
+
+            DATA.Add("oauth_signature", GenerateSignature(Full_URL, DATA));
+            string oAuthHeader = GenerateOAuthHeader(DATA);
+            var formData = new FormUrlEncodedContent(DATA.Where(kvp => !kvp.Key.StartsWith("oauth_")));
+
+            return SEND_REQUEST(Full_URL, oAuthHeader, formData);
+        }
+        string GenerateSignature(string URL, Dictionary<string, string> DATA)
+        {
+            var SigString = string.Join("&", DATA.Union(DATA).Select(kvp => string.Format("{0}={1}", Uri.EscapeDataString(kvp.Key),
+                Uri.EscapeDataString(kvp.Value))).OrderBy(s => s));
+
+            var FULL_SIGDATA=string.Format("{0}&{1}&{2}","POST", Uri.EscapeDataString(URL),
+            Uri.EscapeDataString(SigString.ToString()));
+            return Convert.ToBase64String(SignatureHashCode.ComputeHash(new ASCIIEncoding().GetBytes(FULL_SIGDATA.ToString())));
+        }
+        string GenerateOAuthHeader(Dictionary<string, string> DATA)
+        {
+            return "OAuth " + string.Join(", ",DATA
+            .Where(kvp => kvp.Key.StartsWith("oauth_"))
+            .Select(kvp => string.Format("{0}=\"{1}\"", Uri.EscapeDataString(kvp.Key), Uri.EscapeDataString(kvp.Value)))
+            .OrderBy(s => s));
+        }
+        async Task<string> SEND_REQUEST(string FUll_URL, string oAuthHeader, FormUrlEncodedContent formData)
+        {
+            using (var http = new HttpClient())
+            {
+                http.DefaultRequestHeaders.Add("Authorization", oAuthHeader);
+
+                var httpResp = await http.PostAsync(FUll_URL, formData);
+                var respBody = await httpResp.Content.ReadAsStringAsync();
+
+                return respBody;
+            }
+        }
+    }
+}
---
+++ TweetApp/TweetApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Twitter;
+
+namespace TweetApp
+{
+    class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new Form1());
+        }
+        class Form1 : Form
+        {
+            
+            Button TweetButton;　//いつも通りのGUIステート
+            TextBox TweetWindow;
+            //string tweet;
+            public Form1()
+            {
+                //this.TweetWindow.Text = tweet;
+                this.TweetWindow = new TextBox();
+                this.TweetWindow.Location = new System.Drawing.Point(50, 70);
+                this.TweetWindow.Size = new System.Drawing.Size(380, 170);
+                this.TweetWindow.Multiline = true; //TextBoxのHeightを変更できるようになる
+                this.TweetWindow.Height = 200;
+                this.Controls.Add(this.TweetWindow);
+
+                this.Width = 500;
+                this.Height = 380;
+                this.Text = "TweetAppTest";
+                this.MaximumSize = this.Size;
+                this.MinimumSize = this.Size;
+
+                this.TweetButton = new Button();
+                this.TweetButton.Text = "Tweet";
+                this.TweetButton.Location = new System.Drawing.Point(10, 10);
+                this.TweetButton.Size = new System.Drawing.Size(120, 40);
+                this.TweetButton.Click += new EventHandler(this.OnClickTweetButton);
+                this.Controls.Add(this.TweetButton);
+            }
+            public void OnClickTweetButton(object sender, EventArgs e)
+            {
+                var twitter = new TwitterAPI("o9Osz8TFS6JIFDsfOQn3UjwXz", "kvaR1DSKoSCQvMwy7IGKa7lCN6pt1wFixLB2lQmiNIm1ma6kPI", 
+                 "3103766516-FbGQouXoqeoSEjKPiXII2NQGyQ9UwsZzrC4enct", "O9DVRdtPKVrRC4YQjmWwlB4CU16JN9DPeK3pextwNUSEX");
+                twitter.Tweet("Hello");
+            }
+        }
+    }
+}
